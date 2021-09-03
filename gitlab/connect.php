cdce2a77f7fd51e22138f9154e401473e6d520aa
Nick <63678404+AlterEgo3@users.noreply.github.com>
2021-09-04 01:00:01 +0300

    Delete gitlab directory

--- gitlab/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection=mysqli_connect('127.0.0.1', 'mysql', 'mysql', 'регистрация пользователей');
-if ($connection==false){
-	echo 'Не удалось подключиться к базе данных!<br>'
-	echo mysql_connect_error();
-	exit();
-}
-
-?>
