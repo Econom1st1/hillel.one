d293e19cbee4ad1f062783036532dc1e7ee63cc7
Nick <63678404+AlterEgo3@users.noreply.github.com>
2021-09-07 22:20:58 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,84 +1,12 @@
 <?php
 declare(strict_types=1);
-class rgb
-{
-    private int $red;
-    private int $green;
-    private int $blue;
 
-    public function __construct(int $red, int $green, int $blue)
-    {
-        $this->setRed($red);
-        $this->setGreen($green);
-        $this->setBlue($blue);
-
-    }
-
-    public function setRed(int $red)
-    {
-        $this->red = $red;
-        if ($red <= 0 and $red >= 255) {
-            exit('Значение не может быть меньше 0 или больше 255');
-        }
-    }
-
-    public function getRed(): int
-    {
-        return $this->red;
-    }
-
-    public function setBlue(int $blue)
-    {
-        $this->blue = $blue;
-        if ($blue <= 0 and $blue >= 255) {
-            exit('Значение не может быть меньше 0 или больше 255');
-        }
-    }
-
-    public function getBlue(): int
-    {
-        return $this->blue;
-    }
-
-    public function setGreen(int $green)
-    {
-        $this->green = $green;
-        if ($green <= 0 and $green >= 255) {
-            exit('Значение не может быть меньше 0 или больше 255');
-        }
-    }
-
-    public function getGreen(): int
-    {
-        return $this->green;
-    }
-
-    public function mix(rgb $color): rgb
-    {
-        return new rgb(
-            intdiv($this->getRed() + $color->getRed(), 2),
-            intdiv($this->getGreen() + $color->getGreen(), 2),
-            intdiv($this->getBlue() + $color->getBlue(), 2)
-        );
-    }
-    public function equals($color): bool
-    {
-        if ($this->getRed() == $color->getRed() &&
-            $this->getGreen() == $color->getGreen() &&
-            $this->getBlue() == $color->getBlue()) {
-        return true;
-        }
-        else{
-            return false;
-        }
-    }
-}
-$one=new rgb(1,1,1);
-$two=new rgb(1, 1,1);
-echo "<pre>";
-var_dump([$one, $two]);
-echo "<pre>";
-$mix=$one->mix($two);
-$equals=$one->equals($two);
-var_dump($equals);
-var_dump($mix);
+require_once 'vendor/autoload.php';
+$usd = new \App\Money\Currency('USD');
+$uah = new \App\Money\Currency('UAH');
+var_dump($usd->equals($uah));
+$one = new \App\Money\Money(10, $usd);
+$two = new \App\Money\Money(10, $uah);
+var_dump($one->equals($two));
+$three = new \App\Money\Money(20, $usd);
+$one->add($three);
---
+++ src/Money.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+namespace App\Money;
+class Money
+{
+    private int|float $amount;
+    private Currency  $currency ;
+
+    public function __construct(int|float $amount, Currency  $currency)
+    {
+        $this->setAmount($amount);
+        $this->setCurrency ($currency );
+    }
+    public function equals(Money $money): bool
+    {
+        return $this->getAmount() == $money->getAmount()
+            && $this->getCurrency ()->getIsocode() == $money->getCurrency ()->getIsocode();
+    }
+    public function add(Money $money)
+    {
+        if (!($this->getCurrency ()->getIsocode() == $money->getCurrency ()->getIsocode())) {
+            echo "Нет такой валюты";
+        }
+        $this->setAmount($this->getAmount() + $money->getAmount());
+    }
+    public function getAmount(): int|float
+    {
+        return $this->amount;
+    }
+    public function getCurrency (): Currency
+    {
+        return $this->currency ;
+    }
+    private function setAmount(int|float $amount)
+    {
+        $this->amount = $amount;
+    }
+    private function setCurrency (Currency  $currency)
+    {
+        $this->currency = $currency;
+    }
+}
