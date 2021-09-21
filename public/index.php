5d134710945861745b69665f46c1f4ad74489f5b
Nick <63678404+AlterEgo3@users.noreply.github.com>
2021-09-21 14:00:24 +0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+try {
+    $pdo = new PDO('pgsql:host=db;dbname=sandbox', 'root', 'secret');
+    $sql = 'CREATE TABLE IF NOT EXISTS projects (
+        project_id   INTEGER PRIMARY KEY,
+        project_name TEXT NOT NULL
+      )';
+    $pdo->exec($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
