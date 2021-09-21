08adddee9be1319451e74a70b1878089cfa5ce07
Nick <63678404+AlterEgo3@users.noreply.github.com>
2021-09-21 13:11:47 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
-//    public function testPadLeft()
-//    {
-//        $this->assertEquals(
-//            '-=-=-James',
-//            Str::padLeft('James', 10, '-=')
-//        );
-//
-//        $this->assertEquals(
-//            '     James',
-//            Str::padLeft('James', 10)
-//        );
-//    }
-    function padLeft(string $value, int $length, string $pad = ' '): string
-    {
-        return str_pad($value, $length, $pad, STR_PAD_LEFT);
-    }
-echo padleft();
+declare(strict_types=1);
+
+try {
+    $pdo = new PDO('pgsql:host=db;dbname=sandbox', 'root', 'secret');
+    $sql = 'CREATE TABLE IF NOT EXISTS projects (
+        project_id   INTEGER PRIMARY KEY,
+        project_name TEXT NOT NULL
+      )';
+    $pdo->exec($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
